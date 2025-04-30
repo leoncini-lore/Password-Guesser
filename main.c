623c7ef1491569b8567d9c5c9a4854eb6ef797a3
leoncini <leoncini@unimore>
2025-04-30 12:20:16 +0200

    Update main.c

--- main.c
+++ main.c
@@ -97,7 +97,6 @@ void get_interval(int* start, int* stop, int id) {
     *stop = *start + quotient + (id < remainder ? 1 : 0);
     //printf("Thread %d: start = %d, stop = %d\n", id, *start, *stop);     
 }
-//pippopappo
 
 // Function to guess the password
 char * guess(const char * password, const char * salt, struct crypt_data *cdata) {
